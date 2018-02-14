e7afbd44c5869a95f4e0b77446cb802c12e07a31
DerekMeyers <dmeyers@bnl.gov>
2018-02-14 11:44:09 -0500

    Update extra_models.py

--- fitting_functions/extra_models.py
+++ fitting_functions/extra_models.py
@@ -1,7 +1,7 @@
 from lmfit.model import Model
 from lmfit.models import guess_from_peak, update_param_vals
 
-from .extra_lineshapes import magnon, zero2Linear, zero2Quad
+from .extra_lineshapes import paramagnon, magnon, zero2Linear, zero2Quad
 
 
 COMMON_DOC = """
